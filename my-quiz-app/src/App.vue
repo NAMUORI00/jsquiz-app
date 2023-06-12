577f67462b36c44c1048961a631283614624cbcc
namuori00 <toolbar@jejunu.ac.kr>
2023-06-12 12:52:56 +0900

    퀴즈 구현
    퀴즈 출력 부분, json 작성

--- my-quiz-app/src/App.vue
+++ my-quiz-app/src/App.vue
@@ -4,16 +4,9 @@ import Quiz from './Quiz.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-      <Quiz /> <!-- Quiz 컴포넌트를 추가합니다. -->
+  <div id="app">
+    <Quiz />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- my-quiz-app/src/Quiz.vue
+++ my-quiz-app/src/Quiz.vue
@@ -1,21 +1,86 @@
 <template>
   <div>
-    <h1 v-if="currentQuestion">{{ currentQuestion.question }}</h1>
-    <ul v-if="currentQuestion">
+    <div class="quiz-header">
+      <span class="quiz-progress">{{ currentQuestionIndex + 1 }} / {{ questions.length }}</span>
+      <h1 v-if="currentQuestion">{{ currentQuestion.question }}</h1>
+    </div>
+    <ul v-if="currentQuestion" class="quiz-answers">
       <li
         v-for="(answer, index) in currentQuestion.answers"
         :key="index"
         @click="checkAnswer(index)"
+        :class="{ 'selected': isAnswerSelected(index) }"
       >
         {{ answer }}
       </li>
     </ul>
-    <div v-if="showResult">
-      <h2>You got {{ correctAnswers }} out of {{ questions.length }} questions correct!</h2>
-      <button @click="restart">Restart Quiz</button>
+    <div v-if="showResult" class="quiz-result">
+    <h2>You got {{ correctAnswers }} out of {{ questions.length }} questions correct!</h2>
+    <button @click="restart">Restart Quiz</button>
+    <div v-if="incorrectAnswers.length > 0">
+        <h3>Incorrect answers:</h3>
+        <table>
+        <thead>
+            <tr>
+            <th>Question</th>
+            <th>Answer</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="index in incorrectAnswers" :key="index">
+            <td>
+                <button @click="currentQuestionIndex = index">{{ index + 1 }}</button>
+            </td>
+            <td>
+                {{ questions[index].question }}
+            </td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
     </div>
   </div>
 </template>
+
+
+<style>
+.quiz-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.quiz-progress {
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 10px;
+}
+
+.quiz-answers {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.quiz-answers li {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+
+.quiz-answers li.selected {
+  border-color: #007aff;
+}
+
+.quiz-result {
+  margin-top: 20px;
+}
+</style>
+
 
 <script>
 import axios from 'axios';
@@ -24,10 +89,12 @@ export default {
   name: 'Quiz',
   data() {
     return {
-      questions: [],
-      currentQuestionIndex: 0,
-      correctAnswers: 0,
-      showResult: false,
+        questions: [],  
+        currentQuestionIndex: 0,
+        correctAnswers: 0,
+        incorrectAnswers: [],
+        showResult: false,
+        selectedAnswerIndex: null,
     };
   },
   computed: {
@@ -44,20 +111,30 @@ export default {
         console.error(error);
       }
     },
-    checkAnswer(index) {
-      if (index === this.currentQuestion.correct) {
-        this.correctAnswers++;
-      }
-      if (this.currentQuestionIndex < this.questions.length - 1) {
-        this.currentQuestionIndex++;
-      } else {
-        this.showResult = true;
-      }
+    async checkAnswer(index) {
+        this.selectedAnswerIndex = index;
+            if (index === this.currentQuestion.correct) {
+            this.correctAnswers++;
+        } else {
+            this.incorrectAnswers.push(this.currentQuestionIndex);
+        }
+        if (this.currentQuestionIndex < this.questions.length - 1) {
+            await this.$nextTick();
+            this.currentQuestionIndex++;
+            this.selectedAnswerIndex = null;
+        } else {
+            await this.$nextTick();
+            this.showResult = true;
+        }
+    },
+    isAnswerSelected(index) {
+        return index === this.selectedAnswerIndex;
     },
     restart() {
-      this.currentQuestionIndex = 0;
-      this.correctAnswers = 0;
-      this.showResult = false;
+        this.currentQuestionIndex = 0;
+        this.correctAnswers = 0;
+        this.showResult = false;
+        this.selectedAnswerIndex = null;
     },
   },
   created() {
@@ -65,3 +142,4 @@ export default {
   },
 };
 </script>
+
